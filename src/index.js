49513732084dacfa36eccdb2d2ce4355464d8cd9
Papazumpa <139676520+Papazumpa@users.noreply.github.com>
2024-09-13 21:36:21 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './App'; // Ensure this path is correct
 import * as serviceWorkerRegistration from './serviceWorkerRegistration'; // Import the service worker registration
 
 ReactDOM.render(
