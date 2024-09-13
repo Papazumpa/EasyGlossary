a10f3916a04f1787928ec99328954f41b3abfcb5
Papazumpa <139676520+Papazumpa@users.noreply.github.com>
2024-09-13 21:14:18 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App'; // Ensure this path is correct
+import App from './App';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'; // Import the service worker registration
 
 ReactDOM.render(
   <React.StrictMode>
@@ -9,3 +10,6 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+// Register the service worker
+serviceWorkerRegistration.register();
