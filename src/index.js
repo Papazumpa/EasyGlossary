444ae61764c414cdc437e0d6b3e4ebe6db849210
Papazumpa <139676520+Papazumpa@users.noreply.github.com>
2024-09-13 22:00:48 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App'; // Ensure this path is correct
-import * as serviceWorkerRegistration from './serviceWorkerRegistration'; // Import the service worker registration
+import App from './App';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 ReactDOM.render(
   <React.StrictMode>
