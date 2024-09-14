16c21b6a28cdc550b21a7848a00177a8b6308a36
Papazumpa <139676520+Papazumpa@users.noreply.github.com>
2024-09-14 15:54:15 +0200

    Create AboutPage.js

---
+++ src/pages/AboutPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function AboutPage() {
+  return (
+    <div>
+      <h1>About This Project</h1>
+      <p>This is a quiz site to upload images and create language quizzes.</p>
+    </div>
+  );
+}
+
+export default AboutPage;
