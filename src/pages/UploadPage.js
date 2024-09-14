742095bf8650a891430fe2f1488e476c1ee1029c
Papazumpa <139676520+Papazumpa@users.noreply.github.com>
2024-09-14 15:53:39 +0200

    Create UploadPage.js

---
+++ src/pages/UploadPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function UploadPage() {
+  return (
+    <div>
+      <h1>Upload Image</h1>
+      <button>Upload</button>
+    </div>
+  );
+}
+
+export default UploadPage;
